bcc6399b8a92f62ad9eb9061a51ea5e5489e936e
ZJ1P <zechjacob2@gmail.com>
2016-02-29 19:32:48 -0600

    Alpha 4

--- app/src/main/java/com/example/zechariah/mathprodigy/Games.java
+++ app/src/main/java/com/example/zechariah/mathprodigy/Games.java
@@ -1,24 +1,34 @@
 package com.example.zechariah.mathprodigy;
 
 import java.util.Random;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.app.Activity;
+
 
 /**
  * Created by Zechariah on 2/10/2016.
  */
-public class Games {
+public class Games extends Activity {
     Random randomGenerator = new Random();
     public void addititon()
     {
-        int a = 0, b = 0, c = 0, u = 0, count = 0;
+        int a = 0, b = 0, c = 0, u = null, count = 0, nu = 0,;
+        String su = null;
         long t= System.currentTimeMillis();
         long end = t+60000;
+        EditText in = (EditText)findViewById(R.id.editText);
+        TextView problem = (TextView)findViewById(R.id.problem);
         while(System.currentTimeMillis() < end)
         {
             a = randomGenerator.nextInt(13);
             b = randomGenerator.nextInt(13);
             c = a + b;
-            //print (a + " + " + b)
-            //receive input u = input
+            problem.setText(a + " + " + b);
+            //when submit clicked
+            su = in.getText().toString();
+            nu = Integer.parseInt(su);
             if(u == c)
                 count++;
         }

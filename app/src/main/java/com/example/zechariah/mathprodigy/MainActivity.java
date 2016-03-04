eb9e6849323d5321338370cc621a450a6faff1ba
ZJ1P <zechjacob2@gmail.com>
2016-03-04 10:47:59 -0600

    Alpha 6

--- app/src/main/java/com/example/zechariah/mathprodigy/MainActivity.java
+++ app/src/main/java/com/example/zechariah/mathprodigy/MainActivity.java
@@ -28,25 +28,32 @@ public class Main extends Activity
         super.onCreate(savedInstanceState);
     }
 
+    int a = 0, b = 0, c = 0, count = 0, nu = 0,;
+    String su = null;
+    EditText in = (EditText)findViewById(R.id.editText);
+    TextView problem = (TextView)findViewById(R.id.problem);
+    Button submit = (Button) findViewById(R.id.submitAnswer);
+    OnClickListener submitBtn = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            su = in.getText().toString();   //Recieves user input
+            nu = Integer.parseInt(su);      //Converts user input to an integer value
+            if (nu == c)
+                count++;
+        }
+    };
     public void addititon()
     {
-        int a = 0, b = 0, c = 0, count = 0, nu = 0,;
-        String su = null;
         long t= System.currentTimeMillis();
         long end = t+60000;
-        EditText in = (EditText)findViewById(R.id.editText);
-        TextView problem = (TextView)findViewById(R.id.problem);
         while(System.currentTimeMillis() < end)
         {
             a = randomGenerator.nextInt(13);
             b = randomGenerator.nextInt(13);
             c = a + b;
             problem.setText(a + " + " + b); //Outputs created problem
-            //when submit clicked
-            su = in.getText().toString();   //Recieves user input
-            nu = Integer.parseInt(su);      //Converts user input to an integer value
-            if(nu == c)
-                count++;
+            submit.setOnClickListener(submitBtn);
         }
     }
+
 }
